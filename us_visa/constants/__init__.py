ccc7c005caf409ca050cd0c27c558ccf2bc1ab1f
tintumon <ttuthms1992@gmail.com>
2025-05-20 15:19:03 +0530

    model push

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -5,7 +5,7 @@ DATABASE_NAME = "US_VISA"
 
 COLLECTION_NAME = "visa_data"
 
-MONGODB_URL_KEY = "mongodb+srv://@cluster0.fhypued.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL_KEY = "mongodb+srv://TINTUEEECEC:TINTUEEECEC@cluster0.fhypued.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
@@ -22,8 +22,8 @@ TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
-AWS_ACCESS_KEY_ID_ENV_KEY = ""
-AWS_SECRET_ACCESS_KEY_ENV_KEY = ""
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
 
 
